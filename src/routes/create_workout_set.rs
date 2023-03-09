3b9b3e4f1c9ae35c8e1775f72dd8baff88c8344f
MakisChristou <makis_christou@protonmail.com>
2023-03-09 16:44:26 +0200

    Authentication required for creating set

--- src/routes/create_workout_set.rs
+++ src/routes/create_workout_set.rs
@@ -1,6 +1,16 @@
+use std::sync::Barrier;
+
 use crate::database::sea_orm_active_enums::Bodypart;
+use crate::database::users;
+use crate::database::users::Entity as Users;
 use crate::database::workout_sets;
-use axum::{Extension, Json};
+use axum::headers::authorization::Bearer;
+use axum::headers::Authorization;
+use axum::http::StatusCode;
+use axum::{Extension, Json, TypedHeader};
+use sea_orm::ColumnTrait;
+use sea_orm::EntityTrait;
+use sea_orm::QueryFilter;
 use sea_orm::{prelude::DateTimeWithTimeZone, DatabaseConnection};
 use sea_orm::{ActiveModelTrait, Set};
 use serde::Deserialize;
@@ -15,18 +25,35 @@ pub struct RequestWorkoutSet {
 }
 
 pub async fn create_workout_set(
+    authorization: TypedHeader<Authorization<Bearer>>,
     Extension(database): Extension<DatabaseConnection>,
     Json(request_workout_set): Json<RequestWorkoutSet>,
-) {
+) -> Result<(), StatusCode> {
+    let token = authorization.token();
+
+    let user = if let Some(user) = Users::find()
+        .filter(users::Column::Token.eq(Some(token)))
+        .one(&database)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+    {
+        user
+    } else {
+        return Err(StatusCode::NETWORK_AUTHENTICATION_REQUIRED);
+    };
+
     let new_workout_set = workout_sets::ActiveModel {
         exercise_name: Set(request_workout_set.exercise_name),
         category: Set(request_workout_set.category),
         reps: Set(request_workout_set.reps),
         weight: Set(request_workout_set.weight),
+        user_id: Set(Some(user.id)),
         ..Default::default()
     };
 
     let result = new_workout_set.save(&database).await.unwrap();
 
     dbg!(result);
+
+    Ok(())
 }
