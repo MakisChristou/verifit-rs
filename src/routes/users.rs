674dafbc070b637f0042bad4bad6f6c7c164e119
MakisChristou <makis_christou@protonmail.com>
2023-03-10 17:56:21 +0200

    Hashing passwords

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -30,7 +30,7 @@ pub async fn create_user(
 ) -> Result<Json<ResponseUser>, StatusCode> {
     let new_user = users::ActiveModel {
         username: Set(request_user.username),
-        password: Set(request_user.password),
+        password: Set(hash_password(request_user.password)?),
         token: Set(Some(String::from("Testtoken"))),
         ..Default::default()
     }
@@ -56,6 +56,9 @@ pub async fn login(
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     if let Some(db_user) = db_user {
+        if !verify_password(request_user.password, &db_user.password)? {
+            return Err(StatusCode::UNAUTHORIZED);
+        }
         let new_token = String::from("qwjhqwehkjhdas");
         let mut user = db_user.into_active_model();
 
@@ -89,4 +92,12 @@ pub async fn logout(
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(())
+}
+
+fn hash_password(password: String) -> Result<String, StatusCode> {
+    bcrypt::hash(password, 14).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+fn verify_password(password: String, hash: &str) -> Result<bool, StatusCode> {
+    bcrypt::verify(password, hash).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
